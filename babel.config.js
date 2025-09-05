43f2ae4ed4384020babd3df029b58ef01dc70faf
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-05 15:15:10 -0300

    feat: setup path alias

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    presets: ["babel-preset-expo"],
+    plugins: [
+        [
+          'module-resolver',
+          {
+            root: '.',
+            alias: {
+              '@components': './src/components',
+              '@hooks': './src/hooks',
+              '@routes': './src/routes',
+              '@screens': './src/screens',
+              '@theme': './src/theme',
+              '@domain': './src/domain',
+              '@types': './src/types',
+              '@utils': './src/utils',
+            },
+          },
+        ],
+      ],
+}
