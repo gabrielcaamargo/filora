ed05e45811b5526da30bf9933764e20ab03200e9
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-05 15:12:48 -0300

    feat: setup expo dev client and app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,40 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+import "tsx/cjs";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  name: "DocuScanApp",
+  slug: "DocuScanApp",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.gabrielcaamargo.DocuScanApp",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    package: "com.gabrielcaamargo.DocuScanApp",
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  plugins: [
+    [
+      "expo-dev-client",
+      {
+        launchMode: "most-recent",
+      },
+    ],
+  ],
+});
