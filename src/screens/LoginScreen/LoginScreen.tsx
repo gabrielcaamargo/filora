d339f056f4951461786fd88f47b3025489f719d2
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-09 13:56:42 -0300

    types: AuthScreenProps<screen>

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,8 +1,9 @@
 import { Text } from "@components";
+import { AuthScreenProps } from "@routes";
 import { themedStyleSheet } from "@theme";
 import { View } from "react-native";
 
-export function LoginScreen() {
+export function LoginScreen({}: AuthScreenProps<"LoginScreen">) {
   return (
     <View style={styles.container}>
       <Text preset="headingLarge" weight="Bold" color="primaryColor">
