5473980fa46b05f4c232349347655e5a8a12d4ae
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-22 20:36:37 -0300

    feat: create getUserProfile query

---
+++ src/database/queries/getUser.ts
@@ -0,0 +1,17 @@
+import firestore from "@react-native-firebase/firestore";
+import { DatabaseCollections } from "../databaseTypes";
+import { getAuth } from "@react-native-firebase/auth";
+
+export async function getUserProfile() {
+  const userId = getAuth().currentUser?.uid;
+
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
+
+  const user = await firestore()
+    .collection(DatabaseCollections.USERS)
+    .doc(userId)
+    .get();
+  return user.data();
+}
