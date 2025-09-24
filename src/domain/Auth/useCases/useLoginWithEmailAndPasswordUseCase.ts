d758eb686fffc0143603b53fcc477529547d1ee5
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-09-24 16:33:40 -0300

    feat: create useLoginWithEmailAndPasswordUseCase

---
+++ src/domain/Auth/useCases/useLoginWithEmailAndPasswordUseCase.ts
@@ -0,0 +1,45 @@
+import { errorHandler, MutationOptions } from "@api";
+import { User } from "../../User/userTypes";
+import { authService, LoginWithEmailAndPasswordParams } from "../authService";
+import { useToast } from "@hooks";
+import { useMutation } from "@tanstack/react-query";
+import { useUserSlice } from "@store";
+
+export function useLoginWithEmailAndPasswordUseCase({
+  onSuccess,
+  onError,
+}: MutationOptions<User>) {
+  const { showToast } = useToast();
+  const { setUser } = useUserSlice();
+
+  const { mutate, isPending } = useMutation<
+    User,
+    Error,
+    LoginWithEmailAndPasswordParams
+  >({
+    mutationFn: authService.loginWithEmailAndPassword,
+    onSuccess: (data) => {
+      showToast(
+        "success",
+        "Login realizado com sucesso",
+        "Você agora pode aproveitar o Filora"
+      );
+      setUser(data);
+      onSuccess?.(data);
+    },
+
+    onError: (error) => {
+      showToast("error", "Erro ao fazer login", errorHandler(error.message));
+      onError?.(errorHandler(error.message));
+    },
+  });
+
+  function login(email: string, password: string) {
+    mutate({ email, password });
+  }
+
+  return {
+    login,
+    isPending,
+  };
+}
